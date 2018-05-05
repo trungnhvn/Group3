3818504b2892b6d0bf0b7a206ad4279bededed69
kaisermtv <kaisermtv@yahoo.com>
2018-05-05 11:19:45 +0700

    sv

--- database.sql
+++ database.sql
@@ -39,6 +39,23 @@ CREATE TABLE [dbo].[tblNews](
 	[News_Date] DATETIME NULL
 )
 
+
+CREATE TABLE [dbo].[tblProduct](
+	[Product_Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Product_Name] NVARCHAR(500) NOT NULL,
+	[Product_Description] NVARCHAR(500) NULL,
+	[Product_Component] NVARCHAR(500) NULL,
+	[Product_Content] NTEXT NULL,
+	[Product_Image] NVARCHAR(500) NULL,
+	[Product_Price] NVARCHAR(100) NULL,
+	[Cat_Id]	INT NULL,
+	[Product_Status] INT NOT NULL,
+	[User_Acount] NVARCHAR(200) NOT NULL FOREIGN KEY REFERENCES tblUser(User_Acount),
+	[Product_EditBy] NVARCHAR(200) NULL FOREIGN KEY REFERENCES tblUser(User_Acount),
+	[Product_EditDate] DATETIME NULL,
+	[Product_Date] DATETIME NULL
+)
+
 CREATE TABLE [dbo].[tblMenu](
 	[Menu_Id]		INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[Menu_Name]		NVARCHAR(500) NOT NULL,
@@ -47,3 +64,25 @@ CREATE TABLE [dbo].[tblMenu](
 	[Menu_Link]		NVARCHAR(500) NULL,
 	[ShotOder]		INT NOT NULL
 )
+
+CREATE TABLE [dbo].[tblPhoto](
+	[Photo_Id]			INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Photo_Name]		NVARCHAR(500) NOT NULL,
+	[Photo_Description]	NTEXT NULL,
+	[Photo_Url]			NVARCHAR(500) NOT NULL,
+	[Photo_Link]		NVARCHAR(500) NOT NULL,
+	[Photo_Group]		NVARCHAR(50) NOT NULL,
+	[Photo_Date]		DATETIME NULL,
+	[ShotOder]			INT NOT NULL
+) 
+
+CREATE TABLE [dbo].[tblContact](
+	[Contact_Id]			INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Contact_Name]			NVARCHAR(100) NOT NULL,
+	[Contact_Description]	NTEXT NULL,
+	[Contact_Phone]			NVARCHAR(20) NULL,
+	[Contact_Email]			NVARCHAR(100) NULL,
+	[Contact_Status]		INT NOT NULL,
+	[Contact_Note]			NTEXT NULL,
+	[Contact_Date]			DATETIME NULL
+) 
